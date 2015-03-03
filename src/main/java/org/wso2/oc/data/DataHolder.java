0c7496ecc5fb49b9a0bc7e85ca276918c53ecbd8
Chaya <chaya@chaya-ThinkPad-T410>
2015-03-03 11:35:45 +0530

    changes

--- src/main/java/org/wso2/oc/data/DataHolder.java
+++ src/main/java/org/wso2/oc/data/DataHolder.java
@@ -5,33 +5,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DataHolder {
-  private static Map<Integer,ServerData> serverStorage = new HashMap<Integer,ServerData>();
+	private static Map<Integer,ServerData> serverStorage = new HashMap<Integer,ServerData>();
 	private static Map<Integer, ArrayList<String>> commands =new HashMap<Integer,ArrayList<String>>();
-  private static int serverCount=1;
-/**
- * @return the serversData
- */
-public static Map<Integer,ServerData> getServersData() {
-	return serverStorage;
-}
-/**
- * @param serversData the serversData to set
- */
-public static void setServersData(Map<Integer,ServerData> serversData) {
-	DataHolder.serverStorage = serversData;
-}
-/**
- * @return the serverCount
- */
-public static int getServerCount() {
-	return serverCount++;
-}
-/**
- * @param serverCount the serverCount to set
- */
-public static void setServerCount(int serverCount) {
-	DataHolder.serverCount = serverCount;
-}
+	private static int serverCount=1;
+	/**
+	 * @return the serversData
+	 */
+	public static Map<Integer,ServerData> getServersData() {
+		return serverStorage;
+	}
+	/**
+	 * @param serversData the serversData to set
+	 */
+	public static void setServersData(Map<Integer,ServerData> serversData) {
+		DataHolder.serverStorage = serversData;
+	}
+	/**
+	 * @return the serverCount
+	 */
+	public static int getServerCount() {
+		return serverCount;
+	}
+	public static int incrementServerCount(){
+		return serverCount++;
+	}
+	/**
+	 * @param serverCount the serverCount to set
+	 */
+	public static void setServerCount(int serverCount) {
+		DataHolder.serverCount = serverCount;
+	}
 	public static void addCommand(int serverId, int commandId) {
 
 		ArrayList<String> serverCommands;
@@ -58,30 +61,30 @@ public static void setServerCount(int serverCount) {
 		}
 	}
 
-public static String[] updateServerData(int serverId,ServerData data){
-	ServerData temp=serverStorage.get(serverId);
-	temp.setFreeMemory(data.getFreeMemory());
-	temp.setIdleCpuUsage(data.getIdleCpuUsage());
-	temp.setSystemCpuUsage(data.getSystemCpuUsage());
-	temp.setUserCpuUsage(data.getUserCpuUsage());
-	temp.setAdminServiceUrl(data.getServerUpTime());
-	temp.setServerUpTime(data.getServerUpTime());
-	temp.setThreadCount(data.getThreadCount());
-	temp.setSystemLoadAverage(data.getSystemLoadAverage());
-	temp.setTimestamp(data.getTimestamp());
-	temp.setTenants(data.getTenants());
-	serverStorage.put(serverId,temp);
+	public static String[] updateServerData(int serverId,ServerData data){
+		ServerData temp=serverStorage.get(serverId);
+		temp.setFreeMemory(data.getFreeMemory());
+		temp.setIdleCpuUsage(data.getIdleCpuUsage());
+		temp.setSystemCpuUsage(data.getSystemCpuUsage());
+		temp.setUserCpuUsage(data.getUserCpuUsage());
+		temp.setAdminServiceUrl(data.getServerUpTime());
+		temp.setServerUpTime(data.getServerUpTime());
+		temp.setThreadCount(data.getThreadCount());
+		temp.setSystemLoadAverage(data.getSystemLoadAverage());
+		temp.setTimestamp(data.getTimestamp());
+		temp.setTenants(data.getTenants());
+		serverStorage.put(serverId,temp);
 
-	String tempArray[];
+		String tempArray[];
 
-	if(commands.get(serverId)!=null) {
-		tempArray = commands.get(serverId).toArray(new String[commands.get(serverId).size()]);
-		commands.get(serverId).clear();
-	}else{
-		tempArray = new String[0];
+		if(commands.get(serverId)!=null) {
+			tempArray = commands.get(serverId).toArray(new String[commands.get(serverId).size()]);
+			commands.get(serverId).clear();
+		}else{
+			tempArray = new String[0];
+		}
+
+		return tempArray;
 	}
-
-	return tempArray;
-}
 
 }
--- src/main/java/org/wso2/oc/internal/OCInternal.java
+++ src/main/java/org/wso2/oc/internal/OCInternal.java
@@ -1,40 +1,23 @@
 package org.wso2.oc.internal;
 
+import org.wso2.oc.data.ServerData;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.wso2.oc.data.DataHolder;
-import org.wso2.oc.data.ServerData;
-
-import java.util.HashMap;
-import java.util.Map;
-
-
+/**
+ * Created by chaya on 3/3/15.
+ */
 @Path("internal/oc/servers/")
-public class OCInternal {
+public interface OCInternal {
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response registerServer(ServerData serverData);
 
-	private static final Log log=LogFactory.getLog(OCInternal.class);
-	 @POST
-	 @Consumes("application/json")
-	 @Produces("application/json")
-	 public Response registerServer(ServerData serverData){
-		 int id= DataHolder.getServerCount();
-		 serverData.setServerId(id);
-		 DataHolder.getServersData().put(id, serverData);
-		 Map<String,Integer> response=new HashMap<String,Integer>();
-		 response.put("serverId",id);
-	     return Response.status(201).entity(response).build();		
-	 }
-	 
-	 @PUT
-	 @Path("/{server_id}")
-	 @Consumes("application/json")
-	 @Produces("application/json")
-	 public Response synchronizeServer(@PathParam("server_id") int server_id,ServerData serverData){
-		 String[] response = DataHolder.updateServerData(server_id,serverData);
-		 return Response.status(201).entity(response).build();
-	 }
-	 
+    @PUT
+    @Path("/{server_id}")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response synchronizeServer(@PathParam("server_id") int server_id,ServerData serverData);
 }
